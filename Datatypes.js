29d0bad28192ced5b8a2bc3814bba093dca55a17
SaiKeerthi77 <keerthisai9708@gmail.com>
2026-07-08 13:30:50 +0530

    Add files via upload
    Week1-Day2-assignment

---
+++ Datatypes.js
@@ -0,0 +1,28 @@
+/* String */
+let  userName = "SaiKeerthi"
+console.log(userName);
+console.log(typeof userName);
+
+/* String */
+
+let organization ="IDS Technology"
+console.log(organization);
+console.log(typeof organization);
+
+/* Number */
+
+let  contactNumber = 1234567891
+console.log(contactNumber)
+console.log(typeof contactNumber);
+
+/* Boolean */
+
+let knowsAutomation =true
+console.log(knowsAutomation);
+console.log(typeof knowsAutomation);
+
+/*Undefined*/
+
+let usesplaywright
+console.log(usesplaywright)
+console.log(typeof usesplaywright);
