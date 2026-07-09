e826c5015aea073125f131c1a56ee7b2e9498de2
SaiKeerthi77 <keerthisai9708@gmail.com>
2026-07-09 14:45:02 +0530

    Add files via upload
    Day3-assignment-Number type

---
+++ number.js
@@ -0,0 +1,11 @@
+function number(){
+    var number = -8
+    if (number > 0){
+        console.log("It is a Positive number",number)
+    }else if(number < 0){
+        console.log("It is a Negative number",number)
+    }else{
+        console.log("It is a neutral",number)
+    }
+    }
+    number()
