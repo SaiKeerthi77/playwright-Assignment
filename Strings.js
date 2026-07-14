d5172f3dd2546bddb09ffcef255e31e323d28bea
SaiKeerthi77 <keerthisai9708@gmail.com>
2026-07-14 14:17:49 +0530

    Add files via upload
    Day 5- Strings example1

---
+++ Strings.js
@@ -0,0 +1,9 @@
+//example 1
+let S = "Hello World"
+
+console.log(S.split("")) //Split the string into an array of words
+
+console.log(S.slice(-5 -1)) //displays the last word in the array 
+
+console.log(S.length) //the length of this word "Hello World"
+
